d2d5dff90b50bd5e46ba7d48c09189d4b6676a40
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2020-10-11 00:24:40 -0300

    Fixed error handler response

--- src/services/errorHandler.js
+++ src/services/errorHandler.js
@@ -2,9 +2,9 @@ module.exports = function (error, _, response, next) {
     if (error) {
         console.error(error);
         response
-            .status(error.statusCode)
+            .status(500)
             .json({
-                error
+                error: error.message
             })
     } else {
         return next();
